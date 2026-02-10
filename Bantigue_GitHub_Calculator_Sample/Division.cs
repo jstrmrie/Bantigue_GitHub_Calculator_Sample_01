88daa035636b30b8837ca15dc996bfa8a86eece8
Jester Martin Poblete <poblete.416877@balagtas.sti.edu.ph>
2026-02-10 19:57:57 +0800

    Feat: Division Function

---
+++ Bantigue_GitHub_Calculator_Sample/Division.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace Bantigue_GitHub_Calculator_Sample
+{
+    internal class Division
+    {
+        double num1;
+        double num2;
+        double result;
+
+        public double divFunction (double num1, double num2) // divides two numbers and return the result.
+        {
+            return this.result = num1 / num2;
+        }
+        
+    }
+}
